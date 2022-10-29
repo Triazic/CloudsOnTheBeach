ceb6e2aee29f0adac8afbe0d7ee5907c0f684887
Oliver Vilé <oliver.vile@wge.com.au>
2022-10-29 16:31:39 +1100

    fuck yeah

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 "use strict";
 console.log("hey");
-const getCanvas = () => document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const getDims = () => {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    return { width, height };
+};
+const fillAll = (colour) => {
+    const { width, height } = getDims();
+    context.fillStyle = colour;
+    context.fillRect(0, 0, width, height);
+};
+fillAll("green");
--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 console.log("hey");
 
-const getCanvas = () => document.getElementById("canvas") as HTMLCanvasElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d")!;
+
+const getDims = () => {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    return {width, height};
+}
+
+const fillAll = (colour:string) => {
+    const {width, height} = getDims();
+    context.fillStyle = colour;
+    context.fillRect(0, 0, width, height);
+}
+
+fillAll("green");
+
